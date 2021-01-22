04a0635cc9b244a8fa0490106a6fe29066459013
Victor P <victor.converto@gmail.com>
2021-01-22 15:32:01 +0200

    added cors

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const shoesRouter = require('./routes');
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public', 'gnr', 'dist')));
 
-app.use('/gnr/show', shoesRouter);
+app.use('/show', shoesRouter);
 
 app.listen('3423', () => {
 	console.log('Server started on 3423');
